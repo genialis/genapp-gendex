317eb8de65e7fcef987292a99bfed5673e16510b
Miha Jenko <mjenko@t-2.net>
2014-07-28 12:55:20 +0200

    Merging ...

--- static/genapp-gendex/app/app.js
+++ static/genapp-gendex/app/app.js
@@ -5,16 +5,15 @@
 var gendex = angular.module('gendex', ['genjs.filters', 'genjs.services', 'gendex.widgets', 'ngRoute', 'ngGrid']);
 
 gendex.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-    	.when('/case/:id', {
-			templateUrl: '/static/genapp-gendex/partials/gendex.html',
-			controller: 'GenDexCtrl'
-		})
-	    .otherwise({
-	        redirectTo: '/',
-	        templateUrl: '/static/genapp-gendex/partials/gendex.html',
-	        controller: GenDexCtrl
-	    });
+    $routeProvider.when('/case/:id', {
+		templateUrl: '/static/genapp-gendex/partials/gendex.html',
+		controller: 'GenDexCtrl'
+	})
+    .otherwise({
+        redirectTo: '/',
+        templateUrl: '/static/genapp-gendex/partials/gendex.html',
+        controller: GenDexCtrl
+	});
 }]);
 
 gendex.config(['$httpProvider', function ($httpProvider) {
